44fa97b3c6574ec041511c9aecfbddb34b233808
Roman <sup.makulin@mail.ru>
2022-10-26 20:36:28 +0300

    порешал задачи

--- Tasks/task001/Program.cs
+++ Tasks/task001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
+// Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
+
+void RepeatElements(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int k = i + 1; k < array.Length; k++)
+        {
+            if (array[i] == array[k]) array[k] = 0;
+        }
+        if (array[i] > 0) System.Console.Write($"{array[i]} ");
+    }
+}
+int[] array = { 1, 2, 3, 5, 1, 5, 3, 10 };
+RepeatElements(array);
+
---
+++ Tasks/task002/Program.cs
@@ -0,0 +1,64 @@
+﻿// 60. Составить частотный словарь элементов двумерного массива
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = new Random().Next(1, 7);
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) System.Console.Write($"{array[i, j]}  ");
+        System.Console.WriteLine();
+    }
+}
+int[] ToOneMassiv(int[,] array)
+{
+    int[] newArr = new int[array.GetLength(0) * array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArr[i] = array[i, j];
+            System.Console.Write($"{newArr[i]} ");
+        }
+    }
+    return newArr;
+}
+void RepeatElements(int[] array)
+{
+    int count = 1;
+    int[] array2 = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int k = array.Length - 1; k <= 0; k--)
+        {
+            if (array[i] == array[k])
+            {
+                array2[i] = count + 1;
+            }
+            else
+            {
+                array2[i] = 1;
+            }
+            System.Console.Write($"{array2[i]}");
+        }
+    }
+}
+
+System.Console.Write("Введите размер массива (количество рядов): ");
+int sizeA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите размер массива (количество столбцов): ");
+int sizeB = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[sizeA, sizeB];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+ToOneMassiv(array);
+System.Console.WriteLine();
+RepeatElements(ToOneMassiv(array));
