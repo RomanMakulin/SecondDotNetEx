b09b47070509a4d459dff9088577cedb00efbd9b
Roman <sup.makulin@mail.ru>
2022-10-28 15:10:47 +0300

    решил задачу на квадрат числа

---
+++ Tasks/task003/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. По двум заданным числам проверить является ли одно квадратом второго 
+
+System.Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+System.Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+int Sqrt(int a, int b)
+{
+    if (b == a*a) System.Console.WriteLine($"Число {b} является квадратом числа {a} ");
+    else System.Console.WriteLine("Не квадрат");
+    return a;
+}
+Sqrt(a, b);
