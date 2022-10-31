3a3a544133f8bd0daf2e9967c8564ff7456350ca
Roman <sup.makulin@mail.ru>
2022-10-31 14:48:21 +0300

    набор произведений чисел от 1 до н

---
+++ Tasks/task011/Program.cs
@@ -0,0 +1,15 @@
+﻿// 15. Написать программу получающую набор произведений чисел от 1 до N.
+// Пример: пусть N = 4, тогда
+// [ 1, 2, 6, 24 ]
+void MultiplicationElements(int[] array)
+{
+    int a = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        a = a * (i + 1);
+        System.Console.Write($"{a} ");
+    }
+}
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+MultiplicationElements(array);
