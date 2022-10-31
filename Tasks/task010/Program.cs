9b6175e0f942eb0b27beb8abb6b5e3f5ed813387
Roman <sup.makulin@mail.ru>
2022-10-31 14:17:16 +0300

    Сумма цифр в числе

---
+++ Tasks/task010/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. Подсчитать сумму цифр в вещественном числе.
+int SumEl(int n, int result)
+{
+    while (n > 0)
+    {
+        result += n % 10;
+        n = n / 10;
+    }
+    return result;
+}
+int n = new Random().Next(30, 1000000);
+int result = 0;
+System.Console.WriteLine($"Дано рандомное число: {n} ");
+System.Console.Write($"Сумма элементов числа: {SumEl(n, result)} ");
