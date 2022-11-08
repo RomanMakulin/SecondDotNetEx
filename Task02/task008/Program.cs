d213da4eaf7def44974cd9281946ee7fb340fbba
Roman <sup.makulin@mail.ru>
2022-11-08 11:20:03 +0300

    check is palindrome

---
+++ Task02/task008/Program.cs
@@ -0,0 +1,20 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом. (+)
+
+int[] numbers = { 2, 2, 1, 2, 2 };
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) System.Console.Write($"{array[i]} ");
+}
+FillArray(numbers);
+bool IsPalindrome(int[] array)
+{
+    for (int i = 0, a = array.Length - 1; i < a; i++, a--)
+    {
+        if (array[i] != array[a]) return false;
+        else System.Console.WriteLine("This number is palindrome.");
+        return true;
+    }
+    return true;
+}
+System.Console.WriteLine();
+IsPalindrome(numbers);
