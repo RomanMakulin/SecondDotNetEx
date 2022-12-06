66c8748a699cc2ff4a450f27c20f1ea9d7fb508d
Roman <sup.makulin@mail.ru>
2022-12-06 14:39:43 +0300

    save

--- Task004/task004/Program.cs
+++ Task004/task004/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Сформировать случайную матрицу m x n, состоящую из нулей и единиц, 
+// причем в каждом столбце число единиц равно номеру столбца.
+
+
+int[,] array = new int[5, 4];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 1);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+void DiagonalSum(int[,] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i <= j) array[i,j] = 1;
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+DiagonalSum(array);
---
+++ Task004/task005/Program.cs
@@ -0,0 +1,30 @@
+﻿// 10. Дано натуральное число N. Написать метод(методы) для формирования массива, элементами которого 
+// являются цифры числа N.
+
+int n = 532131;
+int count = 0;
+
+int CountN(int n, int count)
+{
+    for (count = 0; n > 1; count++) n = n / 10;
+    return count;
+}
+
+System.Console.WriteLine($"{CountN(n, count)}");
+System.Console.WriteLine();
+NewArrN(n, CountN(n, count));
+
+void NewArrN(int n, int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = n % 10;
+        n = n / 10;
+    }
+    while (count > 0)
+    {
+        System.Console.Write($"{array[count - 1]} \t");
+        count--;
+    }
+}
