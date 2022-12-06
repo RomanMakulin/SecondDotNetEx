860cadeb4d63f60685972f5753a99513df260a28
Roman <sup.makulin@mail.ru>
2022-12-06 14:39:26 +0300

    сортировка столбцов

---
+++ Task004/task007/Program.cs
@@ -0,0 +1,51 @@
+﻿// 13. Отсотрировать стобцы матрицы по возрастанию и убыванию значений эементов
+
+int[,] array = new int[4, 4];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+void Filter(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = i + 1; k < array.GetLength(0); k++)
+            {
+                if (array[i, j] > array[k, j])
+                {
+                    temp = array[k, j];
+                    array[k, j] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+            System.Console.Write($"{array[i, j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+Filter(array);
